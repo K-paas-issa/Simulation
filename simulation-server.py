80e0aef4e791c3d77cf59e9dcc65712e1e5ebc30
icecoff22 <icecoff2222@gmail.com>
2024-10-21 16:51:50 +0900

    refactor : lifespan 주석처리

--- simulation-server.py
+++ simulation-server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, BackgroundTasks
 from scheduler import scheduler
-from contextlib import asynccontextmanager
+# from contextlib import asynccontextmanager
 import simulation_service
 from fastapi import status
 from fastapi import Response
@@ -13,10 +13,10 @@ def get_climate_data(background_tasks: BackgroundTasks):
     background_tasks.add_task(simulation_service.simulation_start())
     return Response(status_code=status.HTTP_202_ACCEPTED)
 
-@asynccontextmanager
-async def lifespan(app: FastAPI): 
-    yield
-    scheduler.shutdown()
+# @asynccontextmanager
+# async def lifespan(app: FastAPI): 
+#     yield
+#     scheduler.shutdown()
 
 @app.get("/climate-data")
 def simulation(object_name: str, background_tasks: BackgroundTasks):
