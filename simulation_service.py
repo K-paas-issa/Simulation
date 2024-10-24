e1b0c804499b1cbdc8e0abd1ebee1732fc606de9
icecoff22 <icecoff2222@gmail.com>
2024-10-24 21:35:14 +0900

    fix : 비동기 제거

--- simulation_service.py
+++ simulation_service.py
@@ -3,7 +3,6 @@ import requests
 import s3utils
 import learning_client
 import tt
-import asyncio
 
 spring_server_base_url = os.getenv('SPRING_SERVER_BASE_URL')
 climate_data_api_key = os.getenv('CLIMATE_DATA_API_KEY')
@@ -40,14 +39,14 @@ def simulation_start():
     simulation_body(data_path)
     return
 
-async def simulation_body(object_name: str) :
+def simulation_body(object_name: str) :
     print('start simulation body')
     file_name = s3utils.download_csv(object_name) # simulation_input.csv로 다운받은 상태.
     print(file_name)
 
     # 실질적으로 이 함수 내부에서 시뮬레이션 하는 코드 작성할 것. csv도 알아서 접근할 것.
     # 이 함수에서 시뮬레이션 돌려서 나온 값을 csv로 저장. csv이름은 simulation_output.csv로 할 것.
-    for i in range(5): #임시로 5번 TODO : 10번으로 바꾸기
+    for i in range(20): #임시로 5번 TODO : 10번으로 바꾸기
         simulation_output = tt.main(file_name)
 
     # simulation_output_ + 현재 시간 + .csv 형식으로 파일 업로드
@@ -60,6 +59,5 @@ async def simulation_body(object_name: str) :
             print('reqeust success')
         else:
             print('request fail')
-        await asyncio.sleep(10)
         
         
